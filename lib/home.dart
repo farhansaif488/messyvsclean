86c61729015faacb25102b22f838086887b6d76f
Farhan Saif <farhansaif488@gmail.com>
2020-12-27 00:40:57 +0600

    feature : cleanVsMessy room detection added

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
+
 import 'dart:io';
 
 class Home extends StatefulWidget {
@@ -23,7 +24,9 @@ class _HomeState extends State<Home> {
     super.initState();
     _loading = true;
 
-    loadModel().then((value) {});
+    loadModel().then((value) {
+      print("XXXXX");
+    });
   }
 
   @override
@@ -32,7 +35,7 @@ class _HomeState extends State<Home> {
     Tflite.close();
   }
 
-  pickImage() async {
+  Future pickImage() async {
     var image = await picker.getImage(source: ImageSource.camera);
     if (image == null) return null;
     setState(() {
@@ -41,9 +44,9 @@ class _HomeState extends State<Home> {
     classifyImage(_image);
   }
 
-  pickGalleryImage() async{
-    var image = await picker.getImage(source: ImageSource.gallery)
-    if(image == null) return null;
+  Future pickGalleryImage() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
 
     setState(() {
       _image = File(image.path);
@@ -51,53 +54,133 @@ class _HomeState extends State<Home> {
     classifyImage(_image);
   }
 
-  classifyImage(File image)async{
+  classifyImage(File image) async {
+    print("gg");
     var output = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 2,
-      threshold: 0.5,
-      imageMean: 127.5,
-      imageStd: 127.5
-      );
-      setState(() {
-        _loading = false;
-        _output = output;
-      });
+        path: image.path,
+        numResults: 2,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _loading = false;
+      _output = output;
+    });
+    print("loading ");
+    print(_loading);
   }
 
-  loadMode() async{
+  Future loadModel() async {
     await Tflite.loadModel(
       model: "assets/model_unquant.tflite",
       labels: "assets/labels.txt",
-      );
+    );
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-    );
+    //initState();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Clean Or Messy"),
+        ),
+        body: Container(
+          padding: EdgeInsets.only(left: 10, right: 10),
+          child: SingleChildScrollView(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //Buttons
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _GroupText('Choose Source:'),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.start,
+                    buttonMinWidth: 150,
+                    layoutBehavior: ButtonBarLayoutBehavior.padded,
+                    buttonPadding: EdgeInsets.symmetric(vertical: 10),
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: pickImage,
+                        child: Text('Cam'),
+                      ),
+                      SizedBox(width: 20),
+                      RaisedButton(
+                        onPressed: pickGalleryImage,
+                        child: Text("Gallery"),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              _SpaceLine(),
+              Center(
+                  child: _loading
+                      ? Container(
+                          width: 300,
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(height: 50),
+                              Image.asset('assets/room.png'),
+                            ],
+                          ),
+                        )
+                      : Container(
+                          child: Column(
+                            children: <Widget>[
+                              _output != null
+                                  ? Container(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      child: Text(
+                                        '${_output[0]['label']}',
+                                        style: TextStyle(
+                                            color: Colors.red, fontSize: 20.0),
+                                      ),
+                                    )
+                                  : Container(),
+                              SizedBox(height: 20),
+                              Container(
+                                height: 250,
+                                child: Image.file(_image),
+                              ),
+                            ],
+                          ),
+                        ))
+            ],
+          )),
+        ));
   }
 }
 
 class _GroupText extends StatelessWidget {
-  const _GroupText({Key key}) : super(key: key);
-
+  //const _GroupText({Key key}) : super(key: key);
+  final String text;
+  const _GroupText(this.text);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+      ),
     );
   }
 }
 
 class _SpaceLine extends StatelessWidget {
-  const _SpaceLine({Key key}) : super(key: key);
+  //const _SpaceLine({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-    );
+    return SizedBox(
+        height: 5,
+        child: Container(
+          color: Colors.grey,
+        ));
   }
 }
